eb843d8736158096268b740bb7fc3302b07927af
Randy Flood <miroku000@gmail.com>
2024-02-28 08:23:22 -0800

    fixed script

--- generatenpchtml.py
+++ generatenpchtml.py
@@ -2,6 +2,31 @@ import os
 import json
 from collections import defaultdict
 
+import os
+
+def rename_dirs_to_lowercase(path):
+    """
+    Rename all subdirectories within the given path to lowercase.
+    
+    Args:
+    - path (str): The path to the directory whose subdirectories will be renamed.
+    """
+    # Iterate over the items in the directory
+    for name in os.listdir(path):
+        # Full path of the item
+        full_path = os.path.join(path, name)
+        
+        # Check if the item is a directory
+        if os.path.isdir(full_path):
+            # Generate the lowercase version of the full path
+            new_full_path = os.path.join(path, name.lower())
+            
+            # Rename the directory if the new name is different
+            if new_full_path != full_path:
+                os.rename(full_path, new_full_path)
+                print(f"Renamed '{full_path}' to '{new_full_path}'")
+
+
 
 def remove_empty_subdirs(dir_path):
     try:
