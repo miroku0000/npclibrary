9208a393ab64e2a9de671801d613868649893d9f
miroku0000 <49042680+miroku0000@users.noreply.github.com>
2024-03-06 20:55:15 -0800

    Update generatenpchtml.py
    add function to clean up pngs and json that don't match

--- generatenpchtml.py
+++ generatenpchtml.py
@@ -3,6 +3,23 @@ import json
 from collections import defaultdict
 
 import os
+def clean_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        # Create sets to hold the base filenames without extensions for quick lookup
+        png_files = {os.path.splitext(f)[0] for f in files if f.endswith('.png')}
+        json_files = {os.path.splitext(f)[0] for f in files if f.endswith('.json')}
+        
+        # Find PNG files without a corresponding JSON file and delete them
+        png_without_json = png_files - json_files
+        for base_name in png_without_json:
+            os.remove(os.path.join(root, base_name + '.png'))
+            print(f"Deleted {os.path.join(root, base_name )} as no corresponding JSON file found.")
+        
+        # Find JSON files without a corresponding PNG file and delete them
+        json_without_png = json_files - png_files
+        for base_name in json_without_png:
+            os.remove(os.path.join(root, base_name + '.json'))
+            print(f"Deleted {os.path.join(root, base_name)} as no corresponding PNG file found.")
 
 def rename_dirs_to_lowercase(path):
     """
@@ -47,6 +64,7 @@ def remove_empty_subdirs(dir_path):
 # Define the directory for reading the data and for writing the HTML files
 data_directory = "npcs"
 output_directory = "npcs"
+clean_directory(output_directory)
 
 # Ensure the output directory exists, create if it does not
 if not os.path.exists(output_directory):
